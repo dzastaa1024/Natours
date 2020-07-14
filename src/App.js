09e360a03808326e716d44df2e565a949cbf9864
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-07-14 12:17:05 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+import "./globalStyles.scss";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
