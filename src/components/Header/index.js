fb47a929e8e259e9fef21e051a62059bb9263ff3
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-07-15 17:16:21 +0100

    refactoring

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Button from "../Button/index";
 import "./styles.scss";
+import "../../sass/base/_typography.scss";
+import "../../sass/base/_animations.scss";
 
-export default function index() {
+const Header = () => {
   return (
     <header className="header">
       <div className="header__logo-box">
@@ -16,10 +19,13 @@ export default function index() {
           <span className="heading-primary--main">Outdoors</span>
           <span className="heading-primary--sub">is where life happends</span>
         </h1>
-        <a href="#" className="btn btn--white btn--animated">
+        <Button text={"Discover our tours"} />
+        {/* <a href="#" className="btn btn--white btn--animated">
           Discover our tours
-        </a>
+        </a> */}
       </div>
     </header>
   );
-}
+};
+
+export default Header;
