51ec6b6f733485d456a41e141b0dae7b9a250731
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-07-15 10:53:18 +0100

    bem

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,21 @@ import "./styles.scss";
 export default function index() {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src="../../img/logo-white.png" alt="Logo" className="logo" />
+      <div className="header__logo-box">
+        <img
+          src="../../img/logo-white.png"
+          alt="Logo"
+          className="header__logo"
+        />
       </div>
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happends</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happends</span>
         </h1>
+        <a href="#" className="btn btn--white btn--animated">
+          Discover our tours
+        </a>
       </div>
     </header>
   );
