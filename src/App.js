9ddb2fe74c238eb77922c76c5d23ee2109415584
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-07-31 20:20:50 +0200

    rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Features from "./components/Features";
 import Tours from "./components/Tours";
+import Stories from "./components/Stories";
+import BookForm from "./components/BookForm";
+import Footer from "./components/Footer";
 import "./globalStyles.scss";
 
 export default function App() {
@@ -12,6 +15,9 @@ export default function App() {
       <About />
       <Features title={"photo1"} content={"content1"} />
       <Tours />
+      <Stories />
+      <BookForm />
+      <Footer />
     </div>
   );
 }
